7bd557b6af3eabeed2c85bd5be7488c76f28b46b
enbytedev <me@enbyte.dev>
2024-07-09 16:22:24 -0700

    Updated app launcher to fix selections when input is entered. Added 2 apps.

--- home/Scripts/appmenu.sh
+++ home/Scripts/appmenu.sh
@@ -11,13 +11,18 @@ declare -A apps=(
     ["File Manager"]="thunar"
     ["Burp Suite"]="java -jar -Xmx6g /home/tommy/BurpSuitePro/burpsuite_pro.jar"
     ["Virtual Machines"]="virt-manager"
+    ["Monero GUI"]="monero-wallet-gui"
+    ["Ledger GUI"]="ledger-live-desktop"
 )
 
 # Construct the list of application names for wofi
 app_list=$(printf "%s\n" "${!apps[@]}")
 
 # Prompt user to select an application
-selected_app=$(echo -e "$app_list" | wofi -dmenu -p "Select Application:")
+selected_app=$(echo -e "$app_list" | wofi --dmenu --prompt "Select Application:")
+
+# Debug: log the selected application
+echo "Selected application: '$selected_app'"
 
 # If an application is selected, execute it
 if [ -n "$selected_app" ]; then
@@ -25,8 +30,11 @@ if [ -n "$selected_app" ]; then
     app_command=${apps["$selected_app"]}
     if [ -n "$app_command" ]; then
         # Execute the selected application
-        $app_command &
+        echo "Executing: $app_command"
+        eval "$app_command" &
     else
         echo "Error: No command found for '$selected_app'."
     fi
+else
+    echo "No application selected."
 fi
